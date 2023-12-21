6277f1045b198b5b9cca8fb9a45683f688baff5e
JayShriR <navrangerameshwar@gmail.com>
2023-12-21 14:27:11 +0530

    New use of useEffect learn

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -1,11 +1,48 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [formData,setFormData]=useState( {firstName:"",lastName:"",email:""} );
+
+   console.log(formData.firstName);
+  function changeHandler(event){
+    setFormData(previousData=>{
+      return{
+        ...previousData,
+        [event.target.name]:event.target.value
+      }
+    })
+  }
   return (
-    <div className="App">
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+       <form >
+        <input 
+          type='text'
+          placeholder='First Name'
+          onChange={changeHandler}
+          name='firstName'
+          className='border'/>
+
+          <br/>
+          <br/>
+        
+          <input 
+          type='text'
+          placeholder='Last Name'
+          onChange={changeHandler}
+          name='lastName'
+          className='border'/>
+
+          <br/>
+          <br/>
+        
+          <input 
+          type='email'
+          placeholder='Email'
+          onChange={changeHandler}
+          name='email'
+          className='border'/>
+
+       </form>
     </div>
   );
 }
